6cfd97014defcf205cce03817bb53813f477ac49
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-09-19 09:51:17 -0700

    store thrus

--- Store.java
+++ Store.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
 public class Store{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
+    DecimalFormat fmt = new DecimalFormat("0.##");
     //declaring all magic numbers
     final double PEANUT_PRICE = 1.72;
-    final double BOOK_PRICE = 9.00;
+    final double BOOK_PRICE = 9.0;
     final double MOVIE_PRICE = 13.97;
     final double BOOK_SHIPPING = 0.95;
     final double MOVIE_SHIPPING = 0.04;
@@ -30,20 +33,28 @@ public class Store{
     System.out.println("movieSub: "+movieSubtotal);
     double peanutSubtotal = peanutNum*PEANUT_PRICE;
     System.out.println("peanutSub: "+peanutSubtotal);
-    double subtotal = bookSubtotal+movieSubtotal+peanutSubtotal;
+    double subtotal = ((double)Math.round((bookSubtotal+movieSubtotal+peanutSubtotal)*100))/100;
     System.out.println("Sub: "+subtotal);
 
     //shipping cost
-    double shipping = (bookNum*BOOK_SHIPPING)+(movieSubtotal*MOVIE_SHIPPING)+
-    (peanutNum*PEANUT_SHIPPING);
+    double shipping = ((double)Math.round(((bookNum*BOOK_SHIPPING)+(movieSubtotal*MOVIE_SHIPPING)+
+    (peanutNum*PEANUT_SHIPPING))*100))/100;
     System.out.println("ship: "+shipping);
 
     //sales tax
-    double tax = (bookSubtotal+movieSubtotal)*SALES_TAX;
+    double tax = ((double)Math.round((bookSubtotal+movieSubtotal)*SALES_TAX*100))/100;
     System.out.println("tax: "+tax);
 
     //grand total
-    double total = Math.round((subtotal+shipping+tax)*100)/100;
+    double total = ((double)Math.round((subtotal+shipping+tax)*100))/100;
     System.out.println("total: "+total);
+
+    //making random variables
+    //int time =
+
+    //Final Recipt
+    System.out.println("Thank you for Shopping at \n Steve's SuperStore");
+    System.out.println();
+
   }
 }
