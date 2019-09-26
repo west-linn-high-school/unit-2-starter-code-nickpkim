ac9854d62731d818cdcdef86cccd9172b7caea93
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-09-26 11:59:10 -0700

    final maybe snowman

--- Snowman.java
+++ Snowman.java
@@ -5,6 +5,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -23,8 +24,26 @@ public class Snowman extends Application {
     middle.setFill(Color.WHITE);
     Circle head = new Circle(BASE_X, BASE_Y-165, BASE_RADIUS*2/5);
     head.setFill(Color.WHITE);
-    Line rightArm = new Line
-    Group root = new Group(base, middle, head, ground);
+    Line rightArm = new Line(160,190,130,220);
+    rightArm.setStroke(Color.BROWN);
+    Line leftArm = new Line(240,190,270,220);
+    leftArm.setStroke(Color.BROWN);
+    Line rightArm2 = new Line(130,220,160,260);
+    rightArm2.setStroke(Color.BROWN);
+    Line leftArm2 = new Line(270,220,240,260);
+    leftArm2.setStroke(Color.BROWN);
+    Polygon nose = new Polygon();
+    nose.getPoints().addAll(new Double[]{
+      200.0, 135.0,
+      200.0, 145.0,
+      170.0, 140.0
+    });
+    Circle eye1 = new Circle(190,125,5);
+    Circle eye2 = new Circle(210,125,5);
+    Rectangle hatBase = new Rectangle(170,105,60,5);
+    Rectangle hatTop = new Rectangle(175,60,50,50);
+    nose.setFill(Color.ORANGE);
+    Group root = new Group(base, middle, head, ground, rightArm, leftArm, rightArm2, leftArm2, nose, eye1, eye2, hatTop, hatBase);
     Scene scene = new Scene(root, 400, 400, Color.LIGHTBLUE);
     stage.setTitle("Snowman");
     stage.setScene(scene);
